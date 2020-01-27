ec9ab272bebae9d88fe579ade771d03d58ae2fcf
RyuseiNomi <kanontonto1010@gmail.com>
2020-01-27 23:39:36 +0900

    [WIP]create menu list

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,11 @@ import styles from "./Header.module.scss"
 const Header = props => (
     <div className={styles["header__title"]}>
         <p className={styles["header__title-text"]}>{props.title}</p>
+        <ul className={styles["header__menu"]}>
+            <li className={styles["header__menu-list"]}>About Me</li>
+            <li className={styles["header__menu-list"]}>Article List</li>
+            <li className={styles["header__menu-list"]}>Contact</li>
+        </ul>
     </div>
 )
 
