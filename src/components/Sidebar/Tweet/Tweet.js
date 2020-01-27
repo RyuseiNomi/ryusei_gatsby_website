ab2525d35ac202851db37e32bcca5e8e730f1505
RyuseiNomi <kanontonto1010@gmail.com>
2020-01-27 22:26:36 +0900

    enable twitter timeline as link

--- src/components/Sidebar/Tweet/Tweet.js
+++ src/components/Sidebar/Tweet/Tweet.js
@@ -2,8 +2,8 @@ import React from "react"
 import styles from "./Tweet.module.scss"
 
 const Tweet = props => (
-    <div class="Tweet">
-      <a class="twitter-timeline" data-lang="ja" data-width="380" href="https://twitter.com/27ma4_ton10?ref_src=twsrc%5Etfw">Tweets by 27ma4_ton10</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <div className="Tweet">
+      <a className="twitter-timeline" data-lang="ja" data-width="380" href="https://twitter.com/27ma4_ton10?ref_src=twsrc%5Etfw">Tweets by 27ma4_ton10</a> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
     </div>
 )
 
