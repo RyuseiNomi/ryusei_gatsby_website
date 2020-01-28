2cd89732005ab595843cf3a0e0cf7e3961fb845b
RyuseiNomi <kanontonto1010@gmail.com>
2020-01-28 21:12:37 +0900

    adjust timeline height

--- src/components/Sidebar/Tweet/Tweet.js
+++ src/components/Sidebar/Tweet/Tweet.js
@@ -3,7 +3,7 @@ import styles from "./Tweet.module.scss"
 
 const Tweet = props => (
     <div className="Tweet">
-      <a className="twitter-timeline" data-lang="ja" data-width="380" href="https://twitter.com/27ma4_ton10?ref_src=twsrc%5Etfw">Tweets by 27ma4_ton10</a> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+      <a className="twitter-timeline" data-lang="ja" data-height="1080" data-width="380" href="https://twitter.com/27ma4_ton10?ref_src=twsrc%5Etfw">Tweets by 27ma4_ton10</a> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
     </div>
 )
 
