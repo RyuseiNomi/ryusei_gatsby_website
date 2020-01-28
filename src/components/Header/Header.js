3fc0967f5bbb36bc8fcf5432d70469fd9c94a754
RyuseiNomi <kanontonto1010@gmail.com>
2020-01-28 09:51:12 +0900

    create a menu list

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,33 @@
-import React from "react"
-import styles from "./Header.module.scss"
+// @flow strict
+import React from "react";
+import { Link } from "gatsby";
+import styles from "./Header.module.scss";
+import { useSiteMetadata } from '../../hooks';
 
-const Header = props => (
-    <div className={styles["header__title"]}>
-        <p className={styles["header__title-text"]}>{props.title}</p>
-        <ul className={styles["header__menu"]}>
-            <li className={styles["header__menu-list"]}>About Me</li>
-            <li className={styles["header__menu-list"]}>Article List</li>
-            <li className={styles["header__menu-list"]}>Contact</li>
-        </ul>
-    </div>
-)
+type Props = {
+  menu: {
+    label: string,
+    path: string
+  }[]
+};
+
+const Header = Props => {
+  const { menu } = useSiteMetadata();
+
+  return (
+    <nav className={styles["header__title"]}>
+      <p className={styles["header__title-text"]}>{Props.title}</p>
+      <ul className={styles["header__menu"]}>
+        {menu.map((item) => (
+          <li className={styles["header__menu-list"]} key={item.path}>
+            <Link to={item.path}>
+            {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Header;
